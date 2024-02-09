7ff3b7e460e218f2aacb93478dfc109da9a37be3
kartik <kartikb.5578@gmail.com>
2024-02-09 15:52:00 +0530

    video nft

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import './App.css';
+import Nav from './components/Nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer} from 'react-toastify';
+import Home from './components/Home';
+import NFTs from './components/NFTs';
+import {marketplace_abi} from "./Abi.js"
+import Create from './components/Create';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import 'react-toastify/dist/ReactToastify.css';
+import Info from './components/Info.jsx';
+
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState("");
+  const [marketplace, setMarketplace]= useState({});
+  const [nftitem, setNFTitem] = useState({})
+
+
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
+      if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload()
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        setLoading(false)
+        let marketplaceAddress = "0xD3b1bC1d6528D4e9b6D980C19d4F9A668bFB00C5";
+       
+
+        const marketplacecontract = new ethers.Contract(
+          marketplaceAddress,
+          marketplace_abi,
+          signer
+        );
+
+       
+
+        //console.log(contract);
+        setMarketplace(marketplacecontract);
+     
+       
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+
+    provider && loadProvider();
+  }, []);
+
+
+
+
+  return (
+    <BrowserRouter>
+    <ToastContainer/>
+    <div className="App min-h-screen">
+      <div className='gradient-bg-welcome h-screen w-screen'>
+      <Nav account={account}/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/all-nft" element={<NFTs marketplace={marketplace} setNFTitem={setNFTitem} />}></Route>
+        <Route path="/create" element={<Create marketplace={marketplace}  />}></Route>
+        <Route path="/info" element={<Info nftitem={nftitem} />}></Route>
+      </Routes>
+      </div>
+    </div>
+  
+    </BrowserRouter>
+  );
+}
+
+export default App;
